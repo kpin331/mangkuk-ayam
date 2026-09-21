437f57742efec7cf8dd1990aed4a96cad532c899
kpin331 <ibnukevin465@gmail.com>
2026-09-21 14:18:19 +0700

    adding html and templates

--- app/app/config/views.py
+++ app/app/config/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def main(request):
     return HttpResponse("hai :D")
+
+def home(request):
+    return render(request, "index.html", {
+        "nama": "kevin"
+    })
